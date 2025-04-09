f01b01d723bd603db0774b4bc983c1cf0bb4ee4a
luanmilagre <luanmilagrereal@gmail.com>
2025-04-08 20:24:25 -0400

    Add files via upload

---
+++ Update.py
@@ -0,0 +1,29 @@
+# Importar a biblioteca
+import mysql.connector
+
+# Conexão com o MySQL
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Luanmilagre.123",
+    database="teste"
+)
+cursor = conexao.cursor()
+
+
+nome_produto = "chocolate"
+valor = 5
+
+# Comando SQL para atualizar dados
+comando = "UPDATE vendas SET valor = %s WHERE nome_produto = %s"
+
+# Dados que serão inseridos no comando
+dados = (valor, nome_produto)
+
+# Executa o comando
+cursor.execute(comando, dados)
+conexao.commit()  # Confirma as alterações no banco de dados
+
+# Fecha a conexão
+cursor.close()
+conexao.close()
